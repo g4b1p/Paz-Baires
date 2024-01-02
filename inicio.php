ce6a3b8bca5ce71a0147e24a7e11345f353fd7d4
g4b1p <plataramirez17@gmail.com>
2024-01-02 14:27:03 -0300

    inserto codigo

---
+++ inicio.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="ICONA.png">
+    <title>Paz Baires</title>
+</head>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@600&display=swap');
+
+    * {
+        font-family: 'Urbanist', sans-serif;
+        margin: 0;
+    }
+
+    .info {
+        background-color: #8ca853;
+        color: rgb(255, 255, 255);
+        padding: 2px;
+    }
+
+
+
+    /*-----header-----*/
+    header {
+        background-color: rgb(255, 255, 255);
+        padding: 5px;
+        height: 60px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    nav a {
+        font-size: 15px;
+        text-decoration: none;
+        margin-right: 25px;
+        color: rgb(0, 0, 0);
+    }
+
+    nav a:hover {
+        color: #9976e0;
+    }
+
+    .active-link {
+        color: #9976e0;
+    }
+
+    nav img {
+        cursor: pointer;
+        transition: transform 0.3s ease;
+    }
+
+    nav img:hover {
+        transform: scale(1.3);
+    }
+
+
+
+    /*-----container principal-----*/
+    .container-principal {
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 0;
+        margin-bottom: 0;
+        max-width: 1200px;
+    }
+
+
+
+    /*-----nuevos ingresos-----*/
+    .carousel-cont {
+        margin-top: 30px;
+        width: 100%;
+        overflow: hidden;
+        /*el carrusel se desplace más allá del borde de la ventana del navegador*/
+    }
+
+    .carousel {
+        display: flex;
+        animation: scroll 10s linear infinite;
+        /*La animacion se ejecutara durante 10s con una interpolacion lineal (transición suave en lugar de un movimiento brusco) y se repetira infinitamente*/
+    }
+
+    .carousel-item {
+        padding: 2px;
+        flex: 0 0 100%;
+        /*para q los elementos no crezcan ni se encoja (flex-grow y flex-shrink son ambos 0), q tengan un ancho del 100%*/
+        width: 100%;
+        height: 50px;
+        font-size: 20px;
+        display: flex;
+        justify-content: center;
+        /*centra horizontalmente el conten*/
+        align-items: center;
+        /*centra verticalmente el conten*/
+        color: rgb(255, 255, 255);
+        background-color: #8ca853;
+    }
+
+    @keyframes scroll {
+
+        /*conjunto de reglas de animación (notación --> @keyframes)*/
+        0% {
+            /*estado inicial*/
+            transform: translateX(0);
+            /*el elemento no tiene ningún desplazamiento horizontal*/
+        }
+
+        100% {
+            /*estado final*/
+            transform: translateX(-100%);
+            /*Fuera de la vista hacia la izq*/
+        }
+    }
+
+    .articulos-container {
+        display: flex;
+    }
+
+    .articulo {
+        margin: 10px;
+        font-size: 15px;
+    }
+
+    .imagen-container {
+        position: relative;
+    }
+
+    .imagen-container button {
+        font-size: 16px;
+        padding: 5px;
+        width: 200px;
+        height: auto;
+        background-color: #8ca853;
+        border: #8ca853;
+        color: #ffffff;
+        cursor: pointer;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        opacity: 0;
+        transition: opacity 0.2s ease-in-out;
+    }
+
+    .imagen-container:hover button {
+        opacity: 1;
+    }
+
+    .favorite-icon {
+        position: absolute;
+        right: 10px;
+        font-size: 35px;
+        cursor: pointer;
+        transition: color 0.3s ease-in-out;
+    }
+
+    .favorite-icon.active {
+        color: #9976e0;
+    }
+
+
+
+    /*-----informacion-----*/
+    .info-container {
+        padding-bottom: 40px;
+        background-color: #8ca853;
+        color: #ffffff;
+        overflow: hidden;
+        font-size: 15px;
+    }
+
+    .mini-info {
+        padding: 15px;
+    }
+
+    .mini-info h3 {
+        padding-top: 15px;
+        padding-bottom: 15px;
+    }
+
+    .fila-info {
+        display: inline-block;
+    }
+
+    .colum-info {
+        display: flex;
+        justify-content: center;
+    }
+
+    .redes {
+        padding-top: 15px;
+    }
+
+
+
+    /*-----footer-----*/
+    .container-footer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .div-fot {
+        padding: 5px;
+        display: flex;
+        align-items: center;
+    }
+
+    .images {
+        cursor: pointer;
+        object-fit: cover;
+        transition: transform 0.3s ease;
+    }
+
+    .images:hover {
+        transform: scale(1.2);
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
+
+<body>
+    <div class="every-header">
+        <div class="info">
+            <center>
+                <h4>COMPRA MÍNIMA PARA ENVIOS A PARTIR DE $20.000</h4>
+            </center>
+        </div>
+
+        <header>
+            <img src="logo.png" style="height: 65px; width: auto;">
+            <nav>
+                <a href="" class="active-link">INICIO</a>
+                <a href="info-compra.html">QUIERO COMPRAR</a>
+                <a href="tienda.html">TIENDA</a>
+                <a href="carrito.html">CARRITO</a>
+            </nav>
+            <nav>
+                <a href="favoritos.html"><img
+                        src="https://static.vecteezy.com/system/resources/previews/001/187/989/original/heart-png.png"
+                        style="height: 27x; width: 29px;"></a>
+                <a href="https://www.instagram.com/pb__accesorios/" target="_blank"><img
+                        src="https://cdn.icon-icons.com/icons2/2066/PNG/512/instagram_icon_125245.png"
+                        style="height: 27x; width: 29px;"></a>
+                <a href="https://www.facebook.com/pazbaires?locale=es_LA" target="_blank"><img
+                        src="https://cdn-icons-png.flaticon.com/256/59/59439.png" style="height: 27x; width: 29px;"></a>
+            </nav>
+        </header>
+    </div>
+
+    <div class="container-principal">
+        <div>
+            <center>
+                <img src="verano24.png" style="height: auto; width: 100%;">
+            </center>
+        </div>
+
+        <div class="nuevos-ingresos">
+            <div class="carousel-cont">
+                <div class="carousel">
+                    <div class="carousel-item">
+                        <h4>NUEVOS INGRESOS!!! NUEVA TEMPORADA VERANO 2023!!!</h4>
+                    </div>
+                    <div class="carousel-item">
+                        <h4>NUEVOS INGRESOS!!! NUEVA TEMPORADA VERANO 2023!!!</h4>
+                    </div>
+                    <div class="carousel-item">
+                        <h4>NUEVOS INGRESOS!!! NUEVA TEMPORADA VERANO 2023!!!</h4>
+                    </div>
+                    <div class="carousel-item">
+                        <h4>NUEVOS INGRESOS!!! NUEVA TEMPORADA VERANO 2023!!!</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="articulos-container">
+                <div class="articulo">
+                    <div class="imagen-container">
+                        <img src="phot1.jpg" style="max-width: 100%; height: auto;">
+                        <a href=""><button>seleccionar opciones</button></a>
+                        <span class="favorite-icon" onclick="addToFavorites('phot1.jpg')">&#9825;</span>
+                    </div>
+                    <h3>razo mediano (morley)</h3>
+                    <h4>$4.000</h4>
+                </div>
+                <div class="articulo">
+                    <div class="imagen-container">
+                        <img src="phot1.jpg" style="max-width: 100%; height: auto;">
+                        <a href=""><button>seleccionar opciones</button></a>
+                        <span class="favorite-icon" onclick="addToFavorites('phot1.jpg')">&#9825;</span>
+                    </div>
+                    <h3>razo mediano (morley)</h3>
+                    <h4>$4.000</h4>
+                </div>
+                <div class="articulo">
+                    <div class="imagen-container">
+                        <img src="phot1.jpg" style="max-width: 100%; height: auto;">
+                        <a href=""><button>seleccionar opciones</button></a>
+                        <span class="favorite-icon" onclick="addToFavorites('phot1.jpg')">&#9825;</span>
+                    </div>
+                    <h3>razo mediano (morley)</h3>
+                    <h4>$4.000</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="info-container">
+            <center>
+                <h1 style="padding: 25px;">INFORMACIÓN</h1>
+            </center>
+            <div class="colum-info">
+                <div style="display: block;">
+                    <div class="fila-info">
+                        <div class="mini-info">
+                            <h2>UBICACIÓN</h2>
+                            <h3>Campana 560 local 23 y 25. <br> Flores, Argentina.</h3>
+                        </div>
+                        <div class="mini-info">
+                            <h2>HORARIOS</h2>
+                            <h3>Lunes a Viernes de 8 a 15hs. <br> Sabados de 9 a 13hs.</h3>
+                        </div>
+                    </div>
+                    <div class="fila-info">
+                        <div class="mini-info">
+                            <h2>SEGUINOS</h2>
+                            <div class="redes">
+                                <a href="https://www.instagram.com/pb__accesorios/" target="_blank"><img
+                                        src="https://cdn.icon-icons.com/icons2/2066/PNG/512/instagram_icon_125245.png"
+                                        style="height: 27x; width: 29px;"></a>
+                                <a href="https://www.facebook.com/pazbaires?locale=es_LA" target="_blank"><img
+                                        src="https://cdn-icons-png.flaticon.com/256/59/59439.png"
+                                        style="height: 27x; width: 29px;"></a>
+                            </div>
+                        </div>
+                        <div class="mini-info">
+                            <h2>CONTACTO</h2>
+                            <h3>Email: pazbaires.adm@gmail.com <br> Celular: +54 11 3156-2087 </h3>
+                        </div>
+                    </div>
+                </div>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325.728595110654!2d-58.477778751894064!3d-34.62595821670545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc993267983a1%3A0x953bd5fab119147!2sPASEO%20DE%20COMPRAS%20MUNDO%20TEXTIL!5e0!3m2!1ses!2sar!4v1695609342084!5m2!1ses!2sar"
+                    width="600" height="300" style="border:0; border-radius: 50px;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container-footer">
+            <div class="div-fot">
+                <img src="ICONA.png" style="height: 70px; width: auto; padding-right: 10px;">
+                <div>
+                    <h4>PAZ BAIRES © Copyright 2023</h4>
+                    <h4>Todos los derechos reservados.</h4>
+                </div>
+            </div>
+            <div class="div-fot">
+                <a href="https://api.whatsapp.com/send?phone=%2B541128506874&text=Hola!%20Quisiera%20hacer%20una%20consulta"
+                    class="whatsapp">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3670/3670051.png" class="images"
+                        style="width: 70px; height: auto; margin: 10px; position: fixed; bottom: 15px; right: 15px;">
+                </a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
