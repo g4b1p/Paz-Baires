bd4f19fda59b46bde08592ba20ab578a077ca420
g4b1p <plataramirez17@gmail.com>
2024-01-02 14:36:19 -0300

    inserto imagenes y carpeta includes

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<footer>
+        <div class="container-footer">
+            <div class="div-fot">
+                <img src="ICONA.png" style="height: 70px; width: auto; padding-right: 10px;">
+                <div>
+                    <h4>PAZ BAIRES © Copyright 2023</h4>
+                    <h4>Todos los derechos reservados.</h4>
+                </div>
+            </div>
+            <div class="div-fot">
+                <a href="https://api.whatsapp.com/send?phone=%2B541128506874&text=Hola!%20Quisiera%20hacer%20una%20consulta"
+                    class="whatsapp">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3670/3670051.png" class="images"
+                        style="width: 70px; height: auto; margin: 10px; position: fixed; bottom: 15px; right: 15px;">
+                </a>
+            </div>
+        </div>
+</footer>
---
+++ includes/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="ICONA.png">
+    <title>Paz Baires</title>
+</head>
