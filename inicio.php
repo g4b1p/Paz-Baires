5f10f0869facd0e8b0d18464acc77af93c713f7b
g4b1p <plataramirez17@gmail.com>
2024-01-02 14:38:50 -0300

    inserto codes includes

--- inicio.php
+++ inicio.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <link rel="icon" href="ICONA.png">
-    <title>Paz Baires</title>
-</head>
+<?php require_once "includes/head.php"; ?>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@600&display=swap');
@@ -353,24 +349,8 @@
         </div>
     </div>
 
-    <footer>
-        <div class="container-footer">
-            <div class="div-fot">
-                <img src="ICONA.png" style="height: 70px; width: auto; padding-right: 10px;">
-                <div>
-                    <h4>PAZ BAIRES © Copyright 2023</h4>
-                    <h4>Todos los derechos reservados.</h4>
-                </div>
-            </div>
-            <div class="div-fot">
-                <a href="https://api.whatsapp.com/send?phone=%2B541128506874&text=Hola!%20Quisiera%20hacer%20una%20consulta"
-                    class="whatsapp">
-                    <img src="https://cdn-icons-png.flaticon.com/512/3670/3670051.png" class="images"
-                        style="width: 70px; height: auto; margin: 10px; position: fixed; bottom: 15px; right: 15px;">
-                </a>
-            </div>
-        </div>
-    </footer>
+    <?php require_once "includes/footer.php"; ?>
+
 </body>
 
 </html>
