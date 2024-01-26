25c7a4d7b595b77384ce65c162645be5d4c5d22d
“gabi” <plataramirez17@gmail.com>
2024-01-26 14:28:17 -0300

    cambios

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,80 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@600&display=swap');
+
+    * {
+        font-family: 'Urbanist', sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    .info {
+        background-color: #906ADB;
+        color: rgb(255, 255, 255);
+        padding: 2px;
+    }
+
+    header {
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 1000;
+        height: auto;
+        background: #fff;
+    }
+
+    #menu_toggle {
+        display: block;
+    }
+
+    .nav {
+        padding: 0 20px;
+        display: flex;
+        background-color: #fff;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .nav_logo {
+        padding: 10px 0;
+    }
+
+    .menu_items {
+        display: flex;
+        list-style: none;
+        gap: 40px;
+        position: fixed;
+        top: 0;
+        width: 200px;
+        background-color: #fff;
+        justify-content: flex-start;
+        height: 100%;
+        left: -100%;
+        padding: 50px 30px 30px;
+        flex-direction: column;
+        transition: all 0.5s ease;
+    }
+
+    .showMenu .menu_items {
+        left: 0;
+    }
+
+    a {
+        color: #333;
+        text-decoration: none;
+    }
+
+    #menu_toggle {
+        width: 30px;
+        cursor: pointer;
+    }
+
+    .menu_items #menu_toggle {
+        position: absolute;
+        top: 25px;
+        right: 25px;
+    }
+</style>
+
 <div class="every-header">
     <div class="info">
         <center>
@@ -6,37 +83,36 @@
     </div>
 
     <header>
-        <nav role="navigation">
-            <div id="menuToggle">
-
-                <input type="checkbox" />
-                <span></span>
-                <span></span>
-                <span></span>
-                
-                <ul id="menu">
-                    <a href="inicio.php"><li>INICIO</li></a>
-                    <a href="quiero-comprar.php"><li>QUIERO COMPRAR</li></a>
-                    <a href="tienda.php"><li>TIENDA</li></a>
-                    <a href="carrito.php"><li>CARRITO</li></a>
-                </ul>
-            </div>
+        <nav class="nav container">
+            <div></div>
+            <a href="home.php"><img src="images/logo.png" height="80" margin="10"></a>
+            <ul class="menu_items">
+                <img src="images/times.svg" alt="timesicon" id="menu_toggle" />
+                <li><a href="home.php" class="nav_link">Inicio</a></li>
+                <li><a href="quiero-comprar.php" class="nav_link">quiero comprar</a></li>
+                <li><a href="tienda.php" class="nav_link">tienda</a></li>
+                <li><a href="carrito.php" class="nav_link">carrito</a></li>
+                <li>
+                    <a href="" class="nav_link" target="_blank"><img src="https://cdn.icon-icons.com/icons2/2066/PNG/512/instagram_icon_125245.png" style="height: 27x; width: 29px; margin-right: 10px;"></a>
+                    <a href="" class="nav_link" target="_blank"><img src="https://cdn-icons-png.flaticon.com/256/59/59439.png" style="height: 27x; width: 29px;"></a>
+                </li>
+            </ul>
+            <img src="images/bars.svg" alt="timesicon" id="menu_toggle" />
         </nav>
 
-        <img src="images/logo.png" style="height: 80px; width: auto; margin-left: 100px;">
-
-        <nav>
-            <a href="https://www.instagram.com/pb__accesorios/" target="_blank"><img
-                        src="https://cdn.icon-icons.com/icons2/2066/PNG/512/instagram_icon_125245.png"
-                        style="height: 27x; width: 29px;"></a>
-            <a href="https://www.facebook.com/pazbaires?locale=es_LA" target="_blank"><img
-                        src="https://cdn-icons-png.flaticon.com/256/59/59439.png" style="height: 27x; width: 29px;"></a>
-        </nav>
+        <center>
+            <hr style="width: 90%">
+            <br />
+        </center>
     </header>
 
-    <center>
-        <hr>
-        <br>
-    </center>
+    <script>
+        const header = document.querySelector("header");
+        const menuToggler = document.querySelectorAll("#menu_toggle");
 
-<div>
+        menuToggler.forEach(toggler => {
+            toggler.addEventListener("click", () => header.classList.toggle("showMenu"));
+        });
+    </script>
+
+    <div>
